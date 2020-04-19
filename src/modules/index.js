d46fe152d4754488974b11f41594765623e1e511
OJ De Quintos <odequintos@yondu.com>
2020-04-19 17:10:38 +0800

    Export all modules and add it to src/modules/index.js

---
+++ src/modules/index.js
@@ -0,0 +1,11 @@
+import Analytics from './Analytics';
+import Dashborad from './Dashboard';
+import Games from './Games';
+import Users from './Users';
+
+export default [
+  Dashborad,
+  Analytics,
+  Games,
+  Users
+];
