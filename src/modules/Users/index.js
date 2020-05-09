46d5cb3211ea606353db49dc6861aab5d15ee159
OJ De Quintos <odequintos@yondu.com>
2020-05-10 07:54:42 +0800

    Use connect to access redux store

--- src/modules/Users/index.js
+++ src/modules/Users/index.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import  { ALL_USERS } from '../../actions';
 
 class Users extends Component {
   render() {
     return (
-      <div>Users Module</div>
+      <div>
+        Users Module
+        <ul>
+          {
+            this.props.users.map(user => (
+              <li key={ user.id }>
+                { user.name }
+              </li>
+            ))
+          }
+        </ul>
+        <button onClick={() => this.props.ALL_USERS()}>Show Users</button>
+      </div>
     )
   }
 }
 
-export default Users;
+const mapStateToProps = (state) => {
+  return {
+    users: state.users
+  }
+}
+
+const mapDispatchToProps = () => {
+  return {
+    ALL_USERS
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps())(Users);
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -3,6 +3,7 @@ const userReducer = (state = [], action) => {
     case 'ALL_USERS':
       return [
         {
+          "id": 1,
           "balance": "$3,946.45",
           "picture": "http://placehold.it/32x32",
           "age": 23,
@@ -12,6 +13,7 @@ const userReducer = (state = [], action) => {
           "email": "birdramsey@nimon.com"
         },
         {
+          "id": 2,
           "balance": "$2,499.49",
           "picture": "http://placehold.it/32x32",
           "age": 31,
@@ -21,6 +23,7 @@ const userReducer = (state = [], action) => {
           "email": "lillianburgess@luxuria.com"
         },
         {
+          "id": 3,
           "balance": "$2,820.18",
           "picture": "http://placehold.it/32x32",
           "age": 34,
@@ -30,6 +33,7 @@ const userReducer = (state = [], action) => {
           "email": "kristiecole@quadeebo.com"
         },
         {
+          "id": 4,
           "balance": "$3,277.32",
           "picture": "http://placehold.it/32x32",
           "age": 30,
@@ -39,6 +43,7 @@ const userReducer = (state = [], action) => {
           "email": "leonorcross@gronk.com"
         },
         {
+          "id": 5,
           "balance": "$1,972.47",
           "picture": "http://placehold.it/32x32",
           "age": 28,
